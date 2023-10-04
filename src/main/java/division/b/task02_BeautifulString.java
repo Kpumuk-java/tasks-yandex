afb37737fb2edd4a8373008beb280a356c282e8e
Kpumuk-java <bag001@yandex.ru>
2023-10-04 13:33:51 +0300

    creat task 3

--- src/main/java/division/b/task02_BeautifulString.java
+++ src/main/java/division/b/task02_BeautifulString.java
@@ -1,10 +1,38 @@
 package division.b;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class task02_BeautifulString {
 
+    int maxLength;
+
+    private int countChange(List<Character> list, int position, int count, int sumChar) {
+        if (position < list.size()) {
+
+        }
+        return 0;
+    }
+
+
+    public void start() {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int changeBuffer = scanner.nextInt();
+            String str = scanner.nextLine();
+            List<Character> list = new ArrayList<>();
+            for (char c : str.toCharArray()) {
+                list.add(c);
+            }
+            maxLength = 0;
+            System.out.println(Math.max(maxLength, countChange(list, 0, changeBuffer, 0)));
+        }
+
+
+    }
 
 
     public static void main(String[] args) {
-
+        task02_BeautifulString task02_beautifulString = new task02_BeautifulString();
     }
 }
---
+++ src/main/java/division/b/task03_CollectorDiego.java
@@ -0,0 +1,16 @@
+package division.b;
+
+public class task03_CollectorDiego {
+
+
+
+    private void start() {
+
+
+    }
+
+    public static void main(String[] args) {
+        task03_CollectorDiego task03_collectorDiego = new task03_CollectorDiego();
+        task03_collectorDiego.start();
+    }
+}
